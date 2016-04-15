83e91c7e794c1c6749b29b5ff107d5bc485b487e
Raul Fernando Goñi <raul@latreach.com>
2016-04-14 19:47:19 -0500

    Initial Commit

---
+++ L2-Comenzando a condificar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  L2-Comenzando a condificar
+//
+//  Created by Raul Fernando C. Goñi on 14/04/16.
+//  Copyright © 2016 NextU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
